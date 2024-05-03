3e3626573ab53dfb5fa86ddad3c55db87af927b7
Fahad Siddiqui <160652105+Faadi15@users.noreply.github.com>
2024-05-04 00:32:01 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,24 @@
+// // =================================ODD & EVEN NUMBERS=================================
+// function Even(num: number): boolean {
+//     return num % 2 === 0;
+// }
+// function Odd(num: number): boolean {
+//     return num % 2 !== 0;
+// }
+// // Test the functions
+// const numberToCheck = 787;
+// if (Even(numberToCheck)) {
+//     console.log(`${numberToCheck} is even Number.`);
+// } else {
+//     console.log(`${numberToCheck} is odd Number.`);
+// }
+function oddeven(nmber) {
+    if (nmber % 2 == 0) {
+        return "Even";
+    }
+    else {
+        return "Odd";
+    }
+}
+var nmber = oddeven(45);
+console.log("The Number is", nmber);
